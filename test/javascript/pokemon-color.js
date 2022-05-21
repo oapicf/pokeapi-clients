9973be941f59d6f85068fea19eb3b87cc72e1abb
Cliffano Subagio <cliffano@gmail.com>
2022-05-21 14:49:53 +1000

    Add JS test with pokemon color API.

---
+++ test/javascript/pokemon-color.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const PokeapiClient = require('pokeapi-client');
+
+describe('pokemon-color', function() {
+  describe('list', function() {
+    it('should respond with error 401 unauthorized when token is invalid', function(done) {
+      var callback = function(error, data, response) {
+        assert.equal(error, null);
+        console.dir(data);
+        assert.equal(typeof data, 'string'); // response schema hasn't been added to spec
+        assert.equal(response.res.statusCode, 200);
+        done();
+      };
+
+      const apiClient = new PokeapiClient.PokemonColorApi();
+      let opts = {
+        'limit': 100,
+        'offset': 1
+      };
+      apiClient.pokemonColorList(opts, callback);
+    });
+  });
+});
